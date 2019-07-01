4bf39a96a130dc93b341f3e75fab0677ae406bc7
Stanislav <sheveliukhin.s@gmail.com>
2019-07-01 06:36:39 -0400

    Components

---
+++ Part_3/vue-project/src/Car.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="car">
+    <h3>Model: {{carName}}</h3>
+    <p>Year: {{carYear}}</p>
+  </div>
+</template>
+
+<script>
+    export default {
+      props: ['carName'],
+        data() {
+          return {
+            //carName: 'Toyota Prius II',
+            carYear: '2006'
+          }
+        }
+    }
+</script>
+
+<style>
+  .car{
+    border: 1px solid red;
+  }
+</style>
+
+<style scoped>
+
+</style>
