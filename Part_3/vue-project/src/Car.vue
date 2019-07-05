568de1674ed7fd1e31f0c462dc5a59c42c7a5c7b
Stanislav <sheveliukhin.s@gmail.com>
2019-07-05 08:36:40 -0400

    Continue part 4

--- Part_3/vue-project/src/Car.vue
+++ Part_3/vue-project/src/Car.vue
@@ -1,25 +1,57 @@
 <template>
   <div class="car">
-    <h3>Model: {{carName}}</h3>
+    <h3>Model: {{carName}} / {{reverseName}}</h3>
     <p>Year: {{carYear}}</p>
+    <buton @click="changeName">Change name</buton>
+    <buton @click="changeCarNameFunc()">Change from parent</buton>
+    <buton @click="updateCounter()">Update Counter</buton>
   </div>
 </template>
 
 <script>
+    import {eventEmitter} from "./main";
+
     export default {
-      props: ['carName'],
-        data() {
-          return {
-            //carName: 'Toyota Prius II',
-            carYear: '2006'
-          }
+      //props: ['carName', 'carYear'],
+     /* props: {
+        carName: String,
+        carYear: Number
+      },*/
+      props: {
+        carName: {
+          type: String,
+          required: true,
+          default: 'Default Car Name'
+        },
+        carYear: {
+          type: Number,
+          required: true,
+          default: 1999
+        },
+        changeCarNameFunc: Function,
+        counter: Number
+      },
+      methods: {
+        changeName(){
+          this.carName = 'BMW E34',
+          this.$emit('isNamechanged', this.carName)
+        },
+        updateCounter(){
+          this.$emit('counterUpdated', this.counter++)
+          eventEmitter.$emit('counter2Updated')
         }
+      },
+      computed: {
+        reverseName(){
+          return this.carName.split('').reverse().join('');
+        }
+      }
     }
 </script>
 
 <style>
   .car{
-    border: 1px solid red;
+    border: 1px solid #1dff0f;
   }
 </style>
 
---
+++ Part_3/vue-project/src/Counter.vue
@@ -0,0 +1,20 @@
+<template>
+    <h1>counter: {{counter}}</h1>
+    <h1>counter2: {{counter2}}</h1>
+</template>
+
+<script>
+    export default {
+      name: "Counter",
+      props: ['counter'],
+      data(){
+        return{
+          counter2: 0
+        }
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
