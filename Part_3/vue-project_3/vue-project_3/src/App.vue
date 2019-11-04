dc8a0a2ddb10d564c11236673d165f8b42e5df27
Stanislav <sheveliukhin.s@gmail.com>
2019-11-04 18:42:18 +0600

    New project created. 2-3 lessons completed again. No errors now

---
+++ Part_3/vue-project_3/vue-project_3/src/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <div id="app">
+    <h1>{{ msg }}</h1>
+    <p>{{carName}}</p>
+    <app-car
+      :carName="carName"
+      :carYear="carYear">
+    </app-car>
+  </div>
+</template>
+
+<script>
+import Car from './Car.vue'
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Hello Stas',
+      carName: 'Toyota Prius - passed from parent component to Car component',
+      carYear: 2006
+    }
+  },
+  components:{
+    AppCar: Car
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ Part_3/vue-project_3/vue-project_3/src/Car.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="car">
+      <h1>Car name: {{carName}} / reversed name: {{reverseCarName}} for validation lesson</h1>
+      <p>Car year: {{carYear}}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+      //props: ['carName', 'carYear'],
+      // props: {
+      //   carName: String,
+      //   carYear: Number
+      // },
+      props:{
+        carName:{
+          type: String,
+          default: 'BiBi'
+        },
+        carYear:{
+          type: Number,
+          default: 1973
+        }
+      },
+      computed: {
+        reverseCarName(){
+          return this.carName.split('').reverse().join('');
+        }
+      }
+    }
+</script>
+
+<style scoped>
+  .car {
+    border: 1px solid black;
+    width: 1000px;
+    border-radius: 10px;
+    background-color: beige;
+    margin: auto;
+    padding: 20px;
+  }
+</style>
