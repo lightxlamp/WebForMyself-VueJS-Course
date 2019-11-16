94baa58bf5616d0a64aa6908b91aeab023dbe54c
Stanislav <sheveliukhin.s@gmail.com>
2019-11-17 02:59:15 +0600

    "Orders" page added. "New Ad" page started

--- Practice_Creating_a_SPA_application/ad-project/src/components/Ads/NewAd.vue
+++ Practice_Creating_a_SPA_application/ad-project/src/components/Ads/NewAd.vue
@@ -1,8 +1,41 @@
 <template>
     <v-container>
         <v-layout row>
-            <v-flex xs12>
-                <h1>Новое объявление</h1>
+            <v-flex xs12 sm6 offset-sm3>
+                <h1 class="text--secondary mb-3">Добавить объявление</h1>
+
+                <v-form v-model="valid" ref="form" lazy-validation>
+                    <v-text-field
+                            name="title"
+                            label="Название"
+                            type="text"
+                            v-model="title"
+                            required
+                            :rules="[v => !!v || 'Название - обязательное поле']"
+                    />
+
+                    <v-textarea
+                            id="password"
+                            name="desctiption"
+                            label="Описание"
+                            type="text"
+                            v-model="description"
+                            multiline
+                            :rules="[v => !!v || 'Описание - обязательное поле']"
+                    />
+                </v-form>
+
+                <v-layout row>
+                    <v-flex xs12>
+                        <v-btn
+                                color="blue-grey"
+                                class="ma-2 white--text"
+                        >
+                            Загрузить изображение
+                            <v-icon right dark>mdi-cloud-upload</v-icon>
+                        </v-btn>
+                    </v-flex>
+                </v-layout>
             </v-flex>
         </v-layout>
     </v-container>
--- Practice_Creating_a_SPA_application/ad-project/src/components/User/Orders.vue
+++ Practice_Creating_a_SPA_application/ad-project/src/components/User/Orders.vue
@@ -1,8 +1,45 @@
 <template>
     <v-container>
         <v-layout row>
-            <v-flex xs12>
-                <h1>Заказы</h1>
+            <v-flex xs12 sm6 offset-sm3>
+                <h1 class="text--secondary mb-3"> Заказы </h1>
+                <v-list
+                        subheader
+                        two-line
+                        flat
+                >
+
+                    <v-list-item-group
+                            multiple
+                    >
+                        <v-list-item
+                            v-for="order in orders"
+                            :key="order.id"
+                        >
+                            <template>
+                                <v-list-item-action>
+                                    <v-checkbox
+                                        color="success"
+                                        :input-value="order.done"
+                                        @change="markDone(order)"
+                                    ></v-checkbox>
+                                </v-list-item-action>
+
+                                <v-list-item-content>
+                                    <v-list-item-title>{{order.name}}</v-list-item-title>
+                                    <v-list-item-subtitle>{{order.phone}}</v-list-item-subtitle>
+                                </v-list-item-content>
+                                <v-list-item-action>
+                                    <v-btn
+                                        :to="'/ad/' + order.adId"
+                                        class="primary"
+                                    >Открыть</v-btn>
+                                </v-list-item-action>
+                            </template>
+                        </v-list-item>
+
+                    </v-list-item-group>
+                </v-list>
             </v-flex>
         </v-layout>
     </v-container>
@@ -11,7 +48,22 @@
 <script>
     export default {
         data () {
-            return {}
+            return {
+                orders: [
+                    {
+                        id: '0504',
+                        name: 'Влад',
+                        phone: '8-921-121-12-12',
+                        adId: '123',
+                        done: false
+                    }
+                ]
+            }
+        },
+        methods: {
+            markDone(order) {
+                order.done = true
+            }
         }
     }
 </script>
